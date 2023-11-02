12a72a2efb99df6647e4e123e7ff4561acefb396
muham <muhammadessamwork@outlook.com>
2023-11-02 21:05:15 +0200

    adds relatives and custom theme for the panel

---
+++ app/Filament/Resources/MaleResidentResource/RelationManagers/RelativesRelationManager.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Filament\Resources\MaleResidentResource\RelationManagers;
+
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\RelationManagers\RelationManager;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class RelativesRelationManager extends RelationManager
+{
+    protected static string $relationship = 'relatives';
+    protected static ?string $inverseRelationship = 'residents';
+    protected static ?string $title = 'التكوين الاسري للمقيم';
+    protected static ?string $label = 'قريب';
+    protected static ?string $pluralLabel = 'الاقارب';
+
+    public function isReadOnly(): bool
+    {
+        return false;
+    }
+
+    public function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('name')
+                    ->required()
+                    ->maxLength(255),
+            ]);
+    }
+
+    public function table(Table $table): Table
+    {
+        return $table
+            ->recordTitleAttribute('name')
+            ->columns([
+                Tables\Columns\TextColumn::make('name'),
+            ])
+            ->filters([
+                //
+            ])
+            ->headerActions([
+                Tables\Actions\CreateAction::make(),
+                Tables\Actions\AttachAction::make(),
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+                Tables\Actions\DetachAction::make(),
+                Tables\Actions\DeleteAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DetachBulkAction::make(),
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+}
