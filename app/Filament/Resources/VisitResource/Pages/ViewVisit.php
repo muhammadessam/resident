9612cf4934aace91fd2ae39ea011c8fa6a69b1d3
muham <muhammadessamwork@outlook.com>
2023-12-26 23:29:15 +0200

    Adds ViewVisit and some table view improvement

--- app/Filament/Resources/VisitResource/Pages/ViewVisit.php
+++ app/Filament/Resources/VisitResource/Pages/ViewVisit.php
@@ -4,9 +4,21 @@ namespace App\Filament\Resources\VisitResource\Pages;
 
 use App\Filament\Resources\VisitResource;
 use Filament\Actions;
+use Filament\Infolists\Components\Section;
+use Filament\Infolists\Components\TextEntry;
+use Filament\Infolists\Infolist;
 use Filament\Resources\Pages\ViewRecord;
 
 class ViewVisit extends ViewRecord
 {
     protected static string $resource = VisitResource::class;
+
+    public function infolist(Infolist $infolist): Infolist
+    {
+        return $infolist->schema([
+            Section::make('معلومات الزيارة')->schema([
+                TextEntry::make('')
+            ])
+        ]);
+    }
 }
